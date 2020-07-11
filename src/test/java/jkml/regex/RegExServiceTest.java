7919d37973b94499939bd73e5087552131af73c5
jkmcl <jkmcl>
2020-07-11 12:43:06 +0800

    Dependency updates

--- src/test/java/jkml/regex/RegExServiceTest.java
+++ src/test/java/jkml/regex/RegExServiceTest.java
@@ -1,6 +1,6 @@
 package jkml.regex;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -10,32 +10,32 @@ public class RegExServiceTest {
 	@Test
 	public void testNormal() {
 		RegExService regexServ = new RegExService();
-		
+
 		Request req = new Request();
 		req.regex = "in(ter)e(haha)?(st)";
 		req.input = "This is interesting";
-		
+
 		Response rsp = regexServ.evaluate(req);
 
         Gson gson = new GsonBuilder().serializeNulls().create();
         String json = gson.toJson(rsp, Response.class);
-        
+
         System.out.println(json);
 	}
-	
+
 	@Test
 	public void testNotFound() {
 		RegExService regexServ = new RegExService();
-		
+
 		Request req = new Request();
 		req.regex = "in(ter)e(haha)?(st)";
 		req.input = "No such thing";
-		
+
 		Response rsp = regexServ.evaluate(req);
 
         Gson gson = new GsonBuilder().serializeNulls().create();
         String json = gson.toJson(rsp, Response.class);
-        
+
         System.out.println(json);
 	}
 
